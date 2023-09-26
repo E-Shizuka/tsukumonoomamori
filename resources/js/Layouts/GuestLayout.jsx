00b29876d682932d2c19be49ee11767bacb1a384
E-Shizuka <snowman.r@gmail.com>
2023-09-26 15:04:02 +0900

    一部デザイン変更

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,14 +3,14 @@ import { Link } from "@inertiajs/react";
 
 export default function Guest({ children }) {
     return (
-        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+        <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg_color_sub">
             <div>
                 <Link href="/">
                     <img src="images/sakura_logo.svg" className="h-16 w-auto" />
                 </Link>
             </div>
 
-            <div className="w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg bg_color_sub">
+            <div className="w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden sm:rounded-lg bg_color_sub">
                 {children}
             </div>
             <style>
