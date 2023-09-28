9e7113c06cb0f87e2290cfa9b8a7f6eb77cab201
E-Shizuka <snowman.r@gmail.com>
2023-09-28 10:47:48 +0900

    デプロイ用の修正

---
+++ public/build/assets/Dashboard-8ef754f6.js
@@ -0,0 +1,19 @@
+import{j as r,a as e}from"./app-a3054ea6.js";import{A as o}from"./AuthenticatedLayout-9dcd34bd.js";import{F as l}from"./footer-c6b22e31.js";import"./Dropdown-5854429e.js";import"./transition-38dc732d.js";function h({auth:s}){return r.jsxs(o,{user:s.user,children:[r.jsx(e,{title:"ご案内"}),r.jsx("div",{className:"py-12",children:r.jsxs("div",{className:"max-w-7xl mx-auto sm:px-6 lg:px-8",children:[r.jsxs("div",{className:"overflow-hidden sm:rounded-lg",children:[r.jsx("h2",{children:"開運の旅へ"}),r.jsx("h3",{children:"【全体の流れ】"}),r.jsx("h4",{children:"癒しのひととき"}),r.jsx("p",{children:"まずは温泉で心を温め、心身をリフレッシュしましょう。泉質の高い温泉は、禊の効果もあるだけでなく、この土地特有のエネルギーを感じることができます。"}),r.jsx("h4",{children:"自然の力を体感"}),r.jsx("p",{children:"〇〇への訪問は、日常のストレスを癒し、自然のパワーを受け取る特別な場所です。ここで自然と調和し、心を浄化しましょう。"}),r.jsx("h4",{children:"地元の美食を堪能"}),r.jsx("p",{children:"旅の中で地元の食材と料理を楽しむことは、この土地のパワーを味わう最良の方法です。地元の水は澄んでおり、野菜はその恵みを受けて育ち、パワースポットのエネルギーを豊かに含んでいます。 旅の思い出を記録旅の神秘的な瞬間を写真に収め、そのパワーを日常生活に持ち帰ることができます。また、旅の思い出の投稿は他の人とシェアすることもできますし、ご自身で公開設定も選択できますので、お気軽にご利用いただけます。"})]}),r.jsxs("div",{className:" overflow-hidden sm:rounded-lg",children:[r.jsx("h3",{children:"【注意事項】"}),r.jsx("p",{children:"開運の旅へ出発する際に、以下の注意事項を守りましょう。これらのアドバイスは、パワースポットを訪れる際に大切なポイントです。"}),r.jsx("p",{children:"服装について"}),r.jsx("p",{children:"パワースポットへ向かう際は、歩きやすい服装を選びましょう。また、お守りは洋服の上から羽織ること"}),r.jsx("p",{children:"パワースポットでの禁止事項について"}),r.jsx("p",{children:"パワースポットにあるものは、神聖なものであり、その場所にとどまるべきです。石ころや葉っぱひとつにも、特別な意味が込められています。これらを外部に持ち出すことは、ルール違反であり、神域への不敬です。自然と神聖な場所を尊重しましょう。"}),r.jsx("p",{children:"常識外れの行動をすると、因果応報の原則に従い、それがあなたに返ってくることがあります。せっかくの開運の旅ですので、パワースポットでの行動には留意し、神様や自然に感謝の気持ちを忘れずに持ちましょう。"}),r.jsx("p",{children:"パワースポット巡りは素晴らしい経験ですが、その場所とその力を尊重し、感謝の心を持って訪れることが大切です。安全で幸せな旅をお楽しみください。"})]}),r.jsx("div",{className:"max-w-7xl mx-auto p-6 lg:p-8 flex flex-col items-center mb-5",children:r.jsx("button",{onClick:()=>{window.location.href=route("first-mission")},className:"custom-button",children:"出かける"})})]})}),r.jsx("style",{children:`
+                    .custom-button {
+                        background-color: #f19072; /* ボタンの背景色 */
+                        color: black; /* ボタンのテキスト色 */
+                        padding: 8px 16px; /* ボタンのパディング */
+                        border-radius: 4px; /* ボタンの角丸 */
+                        border: none; /* ボーダーなし */
+                        margin: 0.5rem;
+                    }
+
+                    .custom-button:hover {
+                        background-color: #d3381c; /* ホバー時の背景色 */
+                        color: white; /* ボタンのテキスト色 */
+                    }
+                    .bg_color_sub {
+                        background-color: #fff1cf;
+                    }
+
+                                `}),r.jsx(l,{})]})}export{h as default};
---
+++ public/build/assets/SecondMission-e5320147.js
@@ -0,0 +1,19 @@
+import{r as c,j as e,a as n}from"./app-a3054ea6.js";import{A as a}from"./AuthenticatedLayout-9dcd34bd.js";import{F as d}from"./footer-c6b22e31.js";import"./Dropdown-5854429e.js";import"./transition-38dc732d.js";function j({auth:t}){const[s,o]=c.useState(null),i=l=>{const r=l.target.files[0];r&&o(URL.createObjectURL(r))};return e.jsxs(a,{user:t.user,children:[e.jsx(n,{title:"自然の力を体感"}),e.jsx("div",{className:"py-12",children:e.jsxs("div",{className:"max-w-7xl mx-auto sm:px-6 lg:px-8",children:[e.jsxs("div",{className:"overflow-hidden sm:rounded-lg",children:[e.jsx("h2",{children:"自然の力を体感"}),e.jsx("p",{children:"〇〇について"}),e.jsx("p",{children:"言い伝え等"}),e.jsx("h3",{children:"ミッション"}),e.jsx("p",{children:"①お参り or 水を飲む or 〇〇を探す"}),e.jsx("p",{children:"ひとことコメント・感想を記録"}),e.jsx("input",{type:"text"}),e.jsx("p",{children:"②写真を撮る"}),e.jsx("input",{type:"file",accept:"image/*",onChange:i}),s&&e.jsxs("div",{children:[e.jsx("h4",{children:"アップロードされた画像プレビュー:"}),e.jsx("img",{src:s,alt:"アップロードされた画像",width:"200"})]})]}),e.jsx("div",{className:"max-w-7xl mx-auto p-6 lg:p-8 flex flex-col items-center mb-5",children:e.jsx("button",{onClick:()=>{window.location.href=route("third-mission")},className:"custom-button",children:"次の体験へ"})})]})}),e.jsx("style",{children:`
+                    .custom-button {
+                        background-color: #f19072; /* ボタンの背景色 */
+                        color: black; /* ボタンのテキスト色 */
+                        padding: 8px 16px; /* ボタンのパディング */
+                        border-radius: 4px; /* ボタンの角丸 */
+                        border: none; /* ボーダーなし */
+                        margin: 0.5rem;
+                    }
+
+                    .custom-button:hover {
+                        background-color: #d3381c; /* ホバー時の背景色 */
+                        color: white; /* ボタンのテキスト色 */
+                    }
+                    .bg_color_sub {
+                        background-color: #fff1cf;
+                    }
+
+                                `}),e.jsx(d,{})]})}export{j as default};
