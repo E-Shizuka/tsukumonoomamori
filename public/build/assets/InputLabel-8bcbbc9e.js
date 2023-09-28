989f588b08e357d7d956eb810315491f1098147e
E-Shizuka <snowman.r@gmail.com>
2023-09-28 14:04:40 +0900

    build

---
+++ public/build/assets/InputLabel-8bcbbc9e.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-27d2f655.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block font-medium text-sm text-gray-700 "+e,children:t||r})}export{o as I};
