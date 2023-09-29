65640dc3a00e24faefbbe454b7850bebf94d2380
E-Shizuka <snowman.r@gmail.com>
2023-09-29 15:08:53 +0900

    デプロイ用buildファイル

---
+++ public/build/assets/AllPlan-e4115d2b.js
@@ -0,0 +1,19 @@
+import{j as s,a as o}from"./app-0c022762.js";import{G as c}from"./GuestLayout-95559841.js";import{A as a}from"./AuthenticatedLayout-39bfcf4f.js";import{F as i}from"./FooterLogin-2821b8dd.js";import{F as d}from"./Footer-414fcca3.js";import"./Dropdown-a0049182.js";import"./transition-fcf57fde.js";function e(){return s.jsxs(s.Fragment,{children:[s.jsx(o,{title:"自然の力を体感"}),s.jsxs("div",{className:"mb-24 bg_color_sub",children:[s.jsx("div",{className:"py-6",children:s.jsxs("div",{className:"card lg:card-side bg-base-100 shadow-xl",children:[s.jsx("figure",{children:s.jsx("img",{src:"../images/oogosha_oosugi.jpg",alt:"natural"})}),s.jsxs("div",{className:"card-body",children:[s.jsx("h2",{className:"card-title",children:"〇〇旅館"}),s.jsx("p",{children:"旅館所在地："}),s.jsx("p",{children:"パワースポット：大杵社の大杉"}),s.jsx("p",{children:"お守り：銀座きもの装"}),s.jsx("div",{className:"card-actions justify-end",children:s.jsx("button",{onClick:()=>{window.location.href=route("dashboard")},className:"custom-button",children:"詳細を見る"})})]})]})}),s.jsx("div",{className:"py-6",children:s.jsxs("div",{className:"card lg:card-side bg-base-100 shadow-xl",children:[s.jsx("figure",{children:s.jsx("img",{src:"../images/oogosha_oosugi.jpg",alt:"natural"})}),s.jsxs("div",{className:"card-body",children:[s.jsx("h2",{className:"card-title",children:"〇〇旅館"}),s.jsx("p",{children:"旅館所在地："}),s.jsx("p",{children:"パワースポット：大杵社の大杉"}),s.jsx("p",{children:"お守り：銀座きもの装"}),s.jsx("div",{className:"card-actions justify-end",children:s.jsx("button",{onClick:()=>{window.location.href=route("dashboard")},className:"custom-button",children:"詳細を見る"})})]})]})})]})]})}function u({auth:r}){return s.jsxs(s.Fragment,{children:[r.user?s.jsx(a,{user:r.user,children:s.jsx(e,{})}):s.jsx(c,{children:s.jsx(e,{})}),s.jsx("style",{children:`
+                    .custom-button {
+                        background-color: #f19072; /* ボタンの背景色 */
+                        color: black; /* ボタンのテキスト色 */
+                        padding: 8px 16px; /* ボタンのパディング */
+                        border-radius: 4px; /* ボタンの角丸 */
+                        border: none; /* ボーダーなし */
+                        margin: 0.5rem;
+                    }
+
+                    .custom-button:hover {
+                        background-color: #d3381c; /* ホバー時の背景色 */
+                        color: white; /* ボタンのテキスト色 */
+                    }
+                    .bg_color_sub {
+                        background-color: #fff1cf;
+                    }
+
+                                `}),r.user?s.jsx(i,{}):s.jsx(d,{})]})}export{u as default};
---
+++ public/build/assets/Welcome-24512842.js
@@ -0,0 +1,22 @@
+import{j as e,d as l,a as n}from"./app-0c022762.js";import{F as o}from"./Footer-414fcca3.js";import{D as r}from"./Dropdown-a0049182.js";import"./transition-fcf57fde.js";function x({auth:i,header:s,phpVersion:t}){return e.jsx(e.Fragment,{children:e.jsxs("div",{className:"min-h-screen bg_color_sub",children:[e.jsx("nav",{className:"bg-white border-b border-gray-100",children:e.jsx("div",{className:"max-w-7xl mx-auto px-4 sm:px-6 lg:px-8",children:e.jsxs("div",{className:"flex justify-between h-16",children:[e.jsx("div",{className:"flex",children:e.jsx("div",{className:"shrink-0 flex items-center",children:e.jsx(l,{href:"/welcome",children:e.jsx("img",{src:"images/sakura_logo.svg",className:"h-9 w-auto",alt:"Sakura Logo"})})})}),e.jsx("div",{className:"sm:hidden flex items-center",children:e.jsx("button",{onClick:()=>{window.location.href=route("login")},type:"button",className:"inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150",children:"ログイン"})}),e.jsx("div",{className:"hidden sm:flex sm:items-center sm:ml-6",children:e.jsx("div",{className:"ml-3 relative",children:e.jsx(r,{children:e.jsx(r.Trigger,{children:e.jsx("span",{className:"inline-flex rounded-md",children:e.jsx("button",{onClick:()=>{window.location.href=route("login")},type:"button",className:"inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150",children:"ログイン"})})})})})})]})})}),s&&e.jsx("header",{className:"shadow",class:"bg_color_sub",children:e.jsx("div",{className:"max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8",children:s})}),e.jsx(n,{title:"着るお守り"}),e.jsxs("div",{className:"relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center selection:bg-red-500 selection:text-white pb-10 bg_color_sub",children:[e.jsx("div",{className:"sm:fixed sm:top-0 sm:right-0 p-6 text-right"}),e.jsxs("div",{className:"max-w-7xl mx-auto p-6 lg:p-8 flex flex-col items-center",children:[e.jsx("div",{className:"mt-2",children:e.jsx("h1",{children:"「着るお守り」を纏い、神秘な力が宿るパワースポットへの開運の旅へ！"})}),e.jsxs("div",{className:"flex w-full",children:[e.jsx("div",{className:"w-1/2 justify-center",style:{backgroundImage:"url('../../images/furisode.jpg')",backgroundSize:"cover",backgroundPosition:"center",height:"15vh"}}),e.jsx("div",{className:"w-1/2 flex flex-col justify-center bg_color_main",children:e.jsx("h1",{className:"ml-2",children:"つくものお守り"})})]}),e.jsxs("div",{className:"mb-8",children:[e.jsx("p",{children:"「着るお守り」には長い年月をかけて磨かれた技術や職人の卓越した技術が息づいています。"}),e.jsx("p",{children:"伝統工芸品には、研究心や向上心、使い手への温かい気持ち、そして品物に対する愛情など、さまざまな想いが込められています。これらの想いが詰まった工芸品には、魂が宿ると言えるでしょう。 日本には道具を100年以上も大切に使い続けることで、その道具自体がつくも神として守り神になるという伝承があります。この伝承を元に私たちは「着るお守り」を作りました。このお守りは、大切に愛着をもって使うことで、お守りに宿る魂が皆様の力になってくれることでしょう。"})]}),!i.user&&e.jsx("div",{className:"mb-16",children:e.jsx("button",{onClick:()=>{window.location.href=route("register")},className:"custom-button",children:"新規登録"})})]})]}),e.jsx("style",{children:`
+                    .custom-button {
+                        background-color: #f19072; /* ボタンの背景色 */
+                        color: black; /* ボタンのテキスト色 */
+                        padding: 8px 16px; /* ボタンのパディング */
+                        border-radius: 4px; /* ボタンの角丸 */
+                        border: none; /* ボーダーなし */
+                        margin: 0.5rem;
+                    }
+
+                    .custom-button:hover {
+                        background-color: #d3381c; /* ホバー時の背景色 */
+                        color: white; /* ボタンのテキスト色 */
+                    }
+                    .bg_color_main {
+                        background-color: #d3381c;
+                        color:white;
+                    }
+                    .bg_color_sub {
+                        background-color: #fff1cf;
+                    }
+                                `}),e.jsx(o,{})]})})}export{x as default};
