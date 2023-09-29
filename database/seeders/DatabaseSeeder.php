0097cda4c2e7857ae405865f3b61849e4f326198
E-Shizuka <snowman.r@gmail.com>
2023-09-29 22:32:27 +0900

    アイコンのリンク先を修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            AdminsTableSeeder::class,
+            CompaniesTableSeeder::class,
+            MissionsTableSeeder::class,
+            OmamorisTableSeeder::class,
+            PlansTableSeeder::class,
+            RyokansTableSeeder::class
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
